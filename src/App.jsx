23b608fb7a5875bba5815ef21fee3eabd190f688
Flames <vibe-coder@flames.com>
2025-11-18 05:56:31 +0000

    Update project prj_a4jk4lkf - 2025-11-18T05:56:31.148Z

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900">
+    <div className="min-h-screen bg-gray-100 bg-texture-light">
       <Hero />
       <Problem />
       <Solution />
